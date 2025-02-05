5d0fce3170bb6886cc98c8ee3700c88f83a46dcc
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2025-02-05 21:31:59 +0530

    leaned hono

---
+++ java/basic/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int n = 4; // Number of rows
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
